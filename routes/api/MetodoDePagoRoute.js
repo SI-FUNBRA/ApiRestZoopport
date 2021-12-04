8c0907ce260b7064ad6cadb930c35966d4de0b4d
FSCB21 <freddystibencb@gmail.com>
2021-12-04 12:27:11 -0500

    heroku config

--- routes/api/MetodoDePagoRoute.js
+++ routes/api/MetodoDePagoRoute.js
@@ -1,5 +1,4 @@
 const router=require('express').Router();
-const DonacionEconomicaModel = require('../../database/Models/DonacionEconomicaModel');
 const MetodoDePagoModel=require('../../database/Models/MetodoDePagoModel');
 
 //Rutas
