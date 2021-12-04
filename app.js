52db2ff54c01f6665f91497bd3a7c6306226318d
FSCB21 <freddystibencb@gmail.com>
2021-12-04 12:24:38 -0500

    heroku config

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const sequelize = require('./database/db');
 
-/* 
-    require('dotenv').config();
- */
 
-/* 
-require('./database/relacionesM-M'); */
+require('dotenv').config();
+
+
 
 // Setting
 const PORT = process.env.PORT || 3000;
--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
-/* require('dotenv').config();
- */
+require('dotenv').config();
+
 
 const sequelize = new Sequelize(
     process.env.DATABASE,
